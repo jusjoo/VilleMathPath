3a5f7575684ac3d25dd612cb9f495ca6d32f5f42
joonas <jusjoo@gmail.com>
2013-02-18 16:46:56 +0200

    added some buttons

--- VExerStub/src/edu/vserver/exercises/mathpath/PathLayout.java
+++ VExerStub/src/edu/vserver/exercises/mathpath/PathLayout.java
@@ -1,6 +1,7 @@
 package edu.vserver.exercises.mathpath;
 
 import com.vaadin.ui.AbsoluteLayout;
+import com.vaadin.ui.Button;
 import com.vaadin.ui.Label;
 
 public class PathLayout extends AbsoluteLayout {
@@ -16,8 +17,22 @@ public class PathLayout extends AbsoluteLayout {
 	    this.setHeight("300px");
 	    
 	    
+	    Button b1 = new Button("9");
+	    Button b2 = new Button("2 + 2");
+	    Button b3 = new Button("3 + 3");
+	    Button b4 = new Button("4 + 4");
+	    
+	    
+	    
 	    label = new Label("Temp");
 		addComponent(label);
+		addComponent(b1, "top:50%; right:50%");
+		addComponent(b2, "top:25%; right:25%");
+		addComponent(b3, "top:75%; right:25%");
+		addComponent(b4, "top:50%; right:25%");
+		
+		
+		
 		
 	}
 	
